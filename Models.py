f994572ec30c761c1029eae32064640a415ffef6
Yousha <yousha6942@gmail.com>
2023-02-06 13:16:28 +0530

    LR and kKNeighbors models

--- Models.py
+++ Models.py
@@ -24,14 +24,45 @@ dummy_vars = dummy_vars[['Divorced', 'Married', 'Single', 'Together', 'Widow']]
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
-model = LogisticRegression(solver='liblinear')
+from sklearn.model_selection import GridSearchCV
 
 X = df.drop(['Marital_Status','Education'],axis=1)
 y = df['Education']
 
 X_train,X_test,Y_train,Y_test = train_test_split(X,y,test_size=0.3,random_state=1)
 
+## Logistic Regression
+model = LogisticRegression(solver='liblinear')
 model.fit(X_train,Y_train)
+model.score(X_test,Y_test)  #59.7%
 
-model.score(X_test,Y_test)
+
+model2 = LogisticRegression(max_iter=10000)
+model2.fit(X_train,Y_train)
+model2.score(X_test,Y_test)  #58.8%
+
+## K neighbors
+knn = KNeighborsClassifier()
+param_grid = {'n_neighbors': np.arange(2,11)}
+knn_gscv =  GridSearchCV(knn, param_grid, cv=5)
+knn_gscv.fit(X,y)
+knn_gscv.best_params_  #9
+
+knn_final = KNeighborsClassifier(n_neighbors=knn_gscv.best_params_['n_neighbors'])
+knn_final.fit(X,y)
+knn_final.score(X,y)  #61%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
